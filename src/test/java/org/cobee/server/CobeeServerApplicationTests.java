a100aa9dc2df77e6c217f1dc423191f902291be6
Jang HeeJu <93791124+hwnooy@users.noreply.github.com>
2025-05-12 03:59:57 +0900

    initial commit

---
+++ src/test/java/org/cobee/server/CobeeServerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.cobee.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CobeeServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
