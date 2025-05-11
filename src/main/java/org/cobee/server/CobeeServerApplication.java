9d920eda7bb220c5e81acf8dcbcbe971f0dd4b32
Jang HeeJu <93791124+hwnooy@users.noreply.github.com>
2025-05-12 03:59:57 +0900

    initial commit

---
+++ src/main/java/org/cobee/server/CobeeServerApplication.java
@@ -0,0 +1,13 @@
+package org.cobee.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CobeeServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CobeeServerApplication.class, args);
+	}
+
+}
