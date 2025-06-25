dd6e94e19cef03b7797414be1c9f0b4e2d9ce2cf
Jang HeeJu <93791124+hwnooy@users.noreply.github.com>
2025-06-25 11:14:57 +0900

    feat:entity 코드 작성
    alarm과 public profile 관계 및 몇개 뺴고 다 작성..

--- src/main/java/org/cobee/server/CobeeServerApplication.java
+++ src/main/java/org/cobee/server/CobeeServerApplication.java
@@ -2,8 +2,10 @@ package org.cobee.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories
 public class CobeeServerApplication {
 
 	public static void main(String[] args) {
