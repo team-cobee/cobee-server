281666b5a3fe2da2307058373c5609d207c700dc
LJYeon12 <juyeon010530@gmail.com>
2025-08-11 21:37:37 +0900

    feat : 회원가입 & 탈퇴 완

--- src/main/java/org/cobee/server/auth/controller/AuthController.java
+++ src/main/java/org/cobee/server/auth/controller/AuthController.java
@@ -2,23 +2,41 @@ package org.cobee.server.auth.controller;
 
 import lombok.RequiredArgsConstructor;
 import org.cobee.server.auth.dto.MemberInfoDto;
+import org.cobee.server.auth.service.AuthService;
 import org.cobee.server.auth.service.PrincipalDetails;
 import org.cobee.server.global.response.ApiResponse;
 import org.cobee.server.member.domain.Member;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 
 @RestController
 @RequestMapping("/auth")
 @RequiredArgsConstructor
 public class AuthController {
 
+    private final AuthService authService;
+
     @GetMapping()
     public ApiResponse<MemberInfoDto> getUserInfo(@AuthenticationPrincipal PrincipalDetails principalDetails) {
         Member member = principalDetails.getMember();
         MemberInfoDto memberInfo = MemberInfoDto.from(member);
         return ApiResponse.success("사용자 정보 조회 성공", "200", memberInfo);
+    }
+
+    @PostMapping("/logout")
+    public ApiResponse<Void> logout(HttpServletRequest request, HttpServletResponse response) {
+        authService.logout(request, response);
+        return ApiResponse.success("로그아웃 성공", "200", null);
+    }
+
+    @DeleteMapping("/withdraw")
+    public ApiResponse<Void> withdraw(@AuthenticationPrincipal PrincipalDetails principalDetails,
+                                     HttpServletRequest request, HttpServletResponse response) {
+        Member member = principalDetails.getMember();
+        authService.withdrawMember(member.getId(), request, response);
+        return ApiResponse.success("회원 탈퇴 성공", "200", null);
     }
 }
