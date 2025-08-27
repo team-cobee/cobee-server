975977890db430ae98bd268fc6be6cec2105906f
LJYeon12 <juyeon010530@gmail.com>
2025-08-27 19:00:44 +0900

    feat: update spring security

--- src/main/java/org/cobee/server/config/SecurityConfig.java
+++ src/main/java/org/cobee/server/config/SecurityConfig.java
@@ -45,8 +45,9 @@ public class SecurityConfig {
 
                 // 인가 규칙 설정
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/", "/home", "/login/**", "/oauth2/**", "/error").permitAll()
+                        .requestMatchers("/", "/home", "/login/**", "/error").permitAll()
                         .requestMatchers("/swagger-ui/**", "/v3/api-docs/**", "/swagger-resources/**").permitAll()
+                        .requestMatchers("/auth/refresh").permitAll()
                         .requestMatchers("/auth/**").authenticated()
                         .anyRequest().permitAll())
 
