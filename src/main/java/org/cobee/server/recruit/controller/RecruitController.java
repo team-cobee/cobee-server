dc26d449b3c640be0702188b55fa122339318d91
hyejin <hyejin66@sookmyung.ac.kr>
2025-08-28 10:16:23 +0900

    feat: 구인글 등록/ 수정 시 지도 관련 주소 컬럼들도 수정할 수 있도록 로직 추가 (#41)

--- src/main/java/org/cobee/server/recruit/controller/RecruitController.java
+++ src/main/java/org/cobee/server/recruit/controller/RecruitController.java
@@ -18,9 +18,10 @@ import java.util.List;
 public class RecruitController {
 
     private final RecruitService recruitService;
+
     @PostMapping("")
     public ApiResponse<RecruitResponse> createRecruitPost(@RequestBody RecruitRequest request,
-                                                          @AuthenticationPrincipal PrincipalDetails principalDetails){
+                                                          @AuthenticationPrincipal PrincipalDetails principalDetails) {
         Long memberId = principalDetails.getMember().getId();
         RecruitResponse result = recruitService.createRecruitPost(request, memberId);
         return ApiResponse.success("구인글 생성 완료", "RECRUIT_CREATED", result);
@@ -29,39 +30,41 @@ public class RecruitController {
     @PutMapping("/{postId}")
     public ApiResponse<RecruitResponse> updateRecruitPost(@RequestBody RecruitRequest request,
                                                           @AuthenticationPrincipal PrincipalDetails principalDetails,
-                                                          @PathVariable(name="postId") Long postId){
+                                                          @PathVariable(name = "postId") Long postId) {
         Long memberId = principalDetails.getMember().getId();
-        RecruitResponse result = recruitService.updateRecruitPost(request,postId,memberId);
+        RecruitResponse result = recruitService.updateRecruitPost(request, postId, memberId);
         return ApiResponse.success("구인글 수정 완료", "RECRUIT_UPDATED", result);
     }
 
     @GetMapping("/{postId}")
-    public ApiResponse<RecruitResponse> getRecruitPost(@PathVariable(name="postId") Long postId){
+    public ApiResponse<RecruitResponse> getRecruitPost(@PathVariable(name = "postId") Long postId) {
         RecruitResponse result = recruitService.getRecruitPost(postId);
-        return ApiResponse.success("postId가 "+postId+"인 post 조회 완료", "RECRUIT_GET_ONE", result);
+        return ApiResponse.success("postId가 " + postId + "인 post 조회 완료", "RECRUIT_GET_ONE", result);
     }
 
     @GetMapping("")
-    public ApiResponse<List<RecruitResponse>> getRecruitPosts(){
+    public ApiResponse<List<RecruitResponse>> getRecruitPosts() {
         List<RecruitResponse> result = recruitService.getAllRecruitPosts();
-        return ApiResponse.success("모든 구인글 조회 완료","RECRUIT_GET_ALL",result);
+        return ApiResponse.success("모든 구인글 조회 완료", "RECRUIT_GET_ALL", result);
     }
 
     @DeleteMapping("/{postId}")
-    public ApiResponse<Boolean> deleteRecruitPost(@PathVariable(name="postId") Long postId){
+    public ApiResponse<Boolean> deleteRecruitPost(@PathVariable(name = "postId") Long postId) {
         Boolean result = recruitService.deleteRecruitPost(postId);
         if (result) {
-            return ApiResponse.success("postId가 "+postId+"인 구인글 삭제 완료", "RECRUIT_DELETED", result);
+            return ApiResponse.success("postId가 " + postId + "인 구인글 삭제 완료", "RECRUIT_DELETED", result);
         } else {
-            return ApiResponse.failure("postId가 "+postId+"인 구인글 삭제 실패", "RECRUIT_DELETED_FAILED", ErrorCode.POST_NOT_FOUND.getMessage(), result);
+            return ApiResponse.failure("postId가 " + postId + "인 구인글 삭제 실패", "RECRUIT_DELETED_FAILED",
+                    ErrorCode.POST_NOT_FOUND.getMessage(), result);
         }
     }
 
-    // TODO : 나의 구인글 조회 API 구현
     @GetMapping("/my")
-    public ApiResponse<List<RecruitResponse>> getMyRecruitPosts(@AuthenticationPrincipal PrincipalDetails principalDetails){
+    public ApiResponse<List<RecruitResponse>> getMyRecruitPosts(
+            @AuthenticationPrincipal PrincipalDetails principalDetails) {
         Long memberId = principalDetails.getMember().getId();
         List<RecruitResponse> result = recruitService.getAllMyPost(memberId);
         return ApiResponse.success("나의 모든 구인글 조회 완료", "MY_RECRUIT_VIEWED", result);
     }
+
 }
