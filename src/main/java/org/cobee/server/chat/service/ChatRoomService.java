cc247ed37fc60503da2ab41f03cd1d38a65d07ed
hyejin <hyejin66@sookmyung.ac.kr>
2025-08-28 10:06:47 +0900

    chore: 불필요한 주석 제거_채팅 관련 (#41)

--- src/main/java/org/cobee/server/chat/service/ChatRoomService.java
+++ src/main/java/org/cobee/server/chat/service/ChatRoomService.java
@@ -1,7 +1,6 @@
 package org.cobee.server.chat.service;
 
 import java.util.List;
-import java.util.Set;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.cobee.server.chat.domain.ChatRoom;
