27433aac287ca6042732b3114d4dbfb9391ae950
LJYeon12 <juyeon010530@gmail.com>
2025-09-13 17:56:50 +0900

    fix: corsConfiguration 수정

--- src/main/java/org/cobee/server/config/SecurityConfig.java
+++ src/main/java/org/cobee/server/config/SecurityConfig.java
@@ -68,7 +68,8 @@ public class SecurityConfig {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
         configuration.setAllowedOrigins(Arrays.asList(
-                "http://localhost:3000/"
+                "http://localhost:3000",
+                "*"
         ));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type", "X-Requested-With", "Accept", "Origin"));
