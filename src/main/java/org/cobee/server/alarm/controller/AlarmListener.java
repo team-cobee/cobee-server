fbbf6f1722bb177a4f4a9dba55baee31c84bdea7
hwnooy <heejudgit@gmail.com>
2025-09-18 19:46:26 +0900

    fix : alarm 코드 문제 해결

--- src/main/java/org/cobee/server/alarm/controller/AlarmListener.java
+++ src/main/java/org/cobee/server/alarm/controller/AlarmListener.java
@@ -22,13 +22,17 @@ public class AlarmListener {
     @Async
     @TransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT)
     public void onCommentCreated(CommentCreatedEvent e) {
-        // 누군가가 내 글에 댓글을 달았을때
+        var title = e.isReply() ? "대댓글 알림" : "새 댓글 알림";
+        var body  = e.isReply() ? "내 댓글에 답글이 달렸어요" : "내 글에 댓글이 달렸어요";
+
         alarmService.createAndSend(new AlarmCreateRequest(
                 e.getFromUserId(),           // fromUserId: 댓글 작성자
                 e.getToUserId(),             // toUserId: 수신자
                 AlarmType.COMMENT,        // 타입
                 AlarmSourceType.COMMENT,  // 출처 타입
-                e.getCommentId()          // 출처 ID(댓글 PK)
+                e.getCommentId() ,// 출처 ID(댓글 PK)
+                title,
+                body
         ));
     }
 
@@ -36,19 +40,24 @@ public class AlarmListener {
     @TransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT)
     public void onApplyCreated(ApplyCreatedEvent e) {
         // 누군가가 지원을 나한테 했을때
-
+        var title = "지원 알림";
+        var body  = "새로운 지원이 도착했습니다.";
         alarmService.createAndSend(new AlarmCreateRequest(
                 e.getFromUserId(),
                 e.getToUserId(),
                 AlarmType.NEW_APPLY,
                 AlarmSourceType.RECRUIT_POST,
-                e.getApplyId()
+                e.getApplyId(),
+                title,
+                body
         ));
     }
 
     @Async
     @TransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT)
     public void onApplyAcceptResult(ApplyAcceptResultEvent e) {
+        var title = e.isAccepted() ? "매칭성공" : "매칭거절";
+        var body  = e.isAccepted() ? "매칭이 성사되었습니다." : "매칭이 거절되었습니다.";
 
         // 구인글 주인이 승인을 했을 때,
         alarmService.createAndSend(new AlarmCreateRequest(
@@ -56,21 +65,9 @@ public class AlarmListener {
                 e.getToUserId(),
                 AlarmType.START_MATCHING,
                 AlarmSourceType.RECRUIT_POST,
-                e.getApplyId()
-        ));
-    }
-
-    @Async
-    @TransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT)
-    public void onChatCreated(ApplyAcceptResultEvent e) {
-
-        // 구인글 주인이 승인을 했을 때,
-        alarmService.createAndSend(new AlarmCreateRequest(
-                e.getFromUserId(),
-                e.getToUserId(),
-                AlarmType.START_MATCHING,
-                AlarmSourceType.RECRUIT_POST,
-                e.getApplyId()
+                e.getApplyId(),
+                title,
+                body
         ));
     }
 }
